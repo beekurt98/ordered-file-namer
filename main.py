0cc68c936104789aab2a0d473ea8bf26f803524d
Bee <beekurt98@gmail.com>
2024-10-05 17:59:28 +0300

    Update main.py
    Added starting_number & a while loop so that the program loops until the user wants to exit

--- main.py
+++ main.py
@@ -6,20 +6,32 @@ A code by Bee Kurt
 https://github.com/beekurt98/ordered-file-namer
 """
 
-path = input(r"Please enter the directory/path of the folder:" + "\n")  # Gets the file path
-name = input("Desired name after EVERY number. Can be left empty:\n")  # Gets the name
-file_ext = input("File extension to search & change the name of. For example, png, jpeg, txt, pdf, etc.:\n")  # The file extension
+exit_program = False
 
-if name != " " or name != "":  # Checks if the name is empty. If not, adds a space before the name.
-    name = " " + name
+while not exit_program:
 
-files = glob.glob(f"{path}\\*.{file_ext}")
-files.sort(key=os.path.getmtime)  # Sorts the files by date
+    path = input(r"Please enter the directory/path of the folder:" + "\n")  # Gets the file path
+    name = input("Desired name after EVERY number. Can be left empty:\n")  # Gets the name
+    file_ext = input("File extension to search & change the name of. For example, png, jpeg, txt, pdf, etc.:\n")  # The file extensions
+    starting_number = int(input("From which number the naming should start? For example, 1, 5, 8. "
+                                "(Good for naming multiple file extension types)\n"))-1
 
-if len(files) == 0:  # Checks if there are any files with the file extension the user gave
-    print(f"There aren't any files with '.{file_ext}' extension.")
-else:
-    for i in range(len(files)):
-        file = files[i]
-        os.rename(file, f'{path}\\{i + 1}{name}.{file_ext}')
-    print("Filename change successful.")
+    if name != " " or name != "":  # Checks if the name is empty. If not, adds a space before the name.
+        name = " " + name
+
+    files = glob.glob(f"{path}\\*.{file_ext}")
+    files.sort(key=os.path.getmtime)  # Sorts the files by date
+
+    if len(files) == 0:  # Checks if there are any files with the file extension the user gave
+        print(f"There aren't any files with '.{file_ext}' extension.")
+    else:
+        for i in range(len(files)):
+            file = files[i]
+            os.rename(file, f'{path}\\{i+starting_number + 1}{name}.{file_ext}')
+        print("Filename change successful.")
+    cont = input("Continue? Y/n\n")
+    if cont == "Y" or cont == "y":
+        pass
+    else:
+        print("Exiting the program...")
+        exit_program = True
